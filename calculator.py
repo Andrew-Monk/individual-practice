940eb62b336781b8c9016b0d033305fe9201768a
Andrew <andrewmonk1991@gmail.com>
2024-01-11 13:39:52 -0600

    start of calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# Make your own calculator using the terminal and inputs and have it calculate for you
+# similar to when asking AI to do math for you.
+
+
+print("Hello, I am your personal calculator, how many numbers would you like to enter?")
+response = int(input())
+print(response)
+
+
+def calculate(response):
+    pass
